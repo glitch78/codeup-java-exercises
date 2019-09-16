87078d56a97c924f808d21c3c8f2dced8f45ada4
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-16 16:26:11 -0500

    add hello world exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,35 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("one");
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "I'm Tired";
+        System.out.println(myString);
+
+        float myNumber = 123L;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x = x + 5;
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+        int x = 3, y = 4;
+
+        y *= x;
+
+        System.out.println(y);
+    }
+}
