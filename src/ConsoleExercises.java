6fafdb5ccf5b74975ad4fde3ed9d7b62a4bb50ac
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-17 12:28:45 -0500

    add update to console exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
            double pi = 3.14159;
+
 //
 //           System.out.println("The value of pi is approximately " + pi);
 //
-//           System.out.format("The value of pi is approximately %s",pi);
-        Scanner input = new Scanner(System.in);
-//        System.out.println("\nEnter an integer");
+//           System.out.format("The value of pi is approximately %.2f.",pi);
+
+//        System.out.println("\nEnter a number");
 //        int num = input.nextInt();
 //        System.out.format("You entered the number: %s", num);
+
 //        System.out.println("\nEnter a word");
 //        String word1 = input.next();
 //        System.out.format("The first word entered %s", word1);
+
 //        System.out.println("\nEnter a word");
 //        String word2 = input.next();
 //        System.out.format("The second word entered %s", word2);
@@ -33,10 +38,15 @@ public class ConsoleExercises {
 //        System.out.format("The area of the room is %s ", area);
 
 
-        System.out.println("Enter length");
+        System.out.println("\nEnter length");
         String lengthStr = input.nextLine();
         System.out.println("Enter width");
         String widthStr = input.nextLine();
+
+        //////// better way to get the length and width///////////////
+        ///////////value of and parsInt works the same here//////////
+//        int length = Integer.parseInt(input.nextLine());
+//        int width = Integer.valueOf(input.nextLine());
 
 
         int width = Integer.parseInt(lengthStr);
