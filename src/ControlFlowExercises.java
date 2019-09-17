c281deba86eb43477493dbf0c2ea98b23c3fa714
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-17 18:46:26 -0500

    add updated control flow exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//
+//        int i = 0;
+////    while (i < 15){
+////        i++;
+////        System.out.println(i);
+////    }
+//        do {
+//
+//            i+=2;
+//            System.out.println(i);
+//        } while (i < 100);
+//
+
+    }
+}
+
+
+//
+//    int i = 2;
+//    double square = Math.pow(i, 2);
+//do {
+//
+//        }while ();
+
