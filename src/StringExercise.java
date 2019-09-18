7106d3e94caf910366ef04d6ab1f95e5d93b9f4b
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-18 12:24:43 -0500

    add string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,23 @@
+import javax.print.DocFlavor;
+
+public class StringExercise {
+    public static void main(String[] args) {
+//        String message = "We don't need no education\n" +
+//                "We don't need no thought control";
+//        System.out.println(message);
+//        String message = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(message);
+//        String message = "In windows, the main drive is usually c:\\";
+//        System.out.println(message);
+
+        String message = "I can do backslashes \\, double backslashes \\\\ and the amazing triple backslashes \\\\\\";
+        System.out.println(message);
+    }
+}
+
+//   I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+//  In windows, the main drive is usually C:\
+//  Check "this" out!, "s inside of "s!
+//  We don't need no education
+//We don't need no thought control
