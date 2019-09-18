c29dab43a6d2f2d57753b94d4096f04188c7434f
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-17 20:10:27 -0500

    add update to console flow exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,31 +3,31 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int decrement = 105;
-        int count = 0;
-        int i = 4;
-    while (i < 15){
-        i++;
-        System.out.println(i);
-    }
-        do {
+//        int decrement = 105;
+//        int count = 0;
+//        int i = 4;
 
-            count +=2;
-            System.out.println( count);
-        } while (count < 100);
-
+//        int square = byTwo * byTwo;
+//    while (i < 15){
+//        i++;
+//        System.out.println(i);
+//    }/////////increment by 2 //////////
+//        do {
+//
+//            count +=2;
+//            System.out.println( count);
+//        } while (count < 100);
+////////////// decrement by 5 ///////////////
+//        do {
+//            decrement-=5;
+//            System.out.println(decrement);
+//        }while (decrement > -10);
+                long byTwo = 2;
         do {
-            decrement-=5;
-            System.out.println(decrement);
-        }while (decrement > -10);
+            System.out.println(byTwo);
+            byTwo *= byTwo;
+        } while (byTwo <   1000000);
     }
 }
 
-
-//
-//    int i = 2;
-//    double square = Math.pow(i, 2);
-//do {
-//
-//        }while ();
 
