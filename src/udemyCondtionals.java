16e66a2500a0f5e934786fa9e08e6fabddd77c67
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-21 13:02:01 -0500

    add update  udemy conditionals

--- src/udemyCondtionals.java
+++ src/udemyCondtionals.java
@@ -1,20 +1,40 @@
-
+import java.util.Scanner;
 
 public class udemyCondtionals {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        Scanner sc = new Scanner(System.in);
 
-        if(10 < 5){
-            System.out.println(4);
-            System.out.println(5);
-            System.out.println(6);
-        }
 
-        System.out.println(7);
-        System.out.println(8);
-        System.out.println(9);
+          ////////// Introduction to if-statements ///////////
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//
+//        if(10 < 5){
+//            System.out.println(4);
+//            System.out.println(5);
+//            System.out.println(6);
+//        }
+//
+//        System.out.println(7);
+//        System.out.println(8);
+//        System.out.println(9);
+
+
+        ///////// If, else-if, and else blocks  ///////////////
+        System.out.println("Welcome to SuperFast Rollercoaster");
+        System.out.print("Please enter your height in inches:");
+        int height = sc.nextInt();
+
+       if(height < 60){
+           System.out.println("Sorry, you're too short");
+       } else if(height > 84) {
+           System.out.println("Sorry, you're too tall");
+       }
+
+       else {
+           System.out.println("Enjoy the ride");
+       }
     }
 }
 
