2effce6833a1c8bd6a6bdf1fb6689692c459c21d
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-21 13:58:58 -0500

    add update  udemy conditionals

--- src/udemyCondtionals.java
+++ src/udemyCondtionals.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Scanner;
 
 public class udemyCondtionals {
@@ -5,7 +7,7 @@ public class udemyCondtionals {
         Scanner sc = new Scanner(System.in);
 
 
-          ////////// Introduction to if-statements ///////////
+        ////////// Introduction to if-statements ///////////
 //        System.out.println(1);
 //        System.out.println(2);
 //        System.out.println(3);
@@ -22,19 +24,42 @@ public class udemyCondtionals {
 
 
         ///////// If, else-if, and else blocks  ///////////////
-        System.out.println("Welcome to SuperFast Rollercoaster");
-        System.out.print("Please enter your height in inches:");
-        int height = sc.nextInt();
+//        System.out.println("Welcome to SuperFast Rollercoaster");
+//        System.out.print("Please enter your height in inches:");
+//        int height = sc.nextInt();
+//
+//       if(height < 60){
+//           System.out.println("Sorry, you're too short");
+//       } else if(height > 84) {
+//           System.out.println("Sorry, you're too tall");
+//       }
+//
+//       else {
+//           System.out.println("Enjoy the ride");
+//       }
+        ////////// Complex conditions with logical operators  /////////
+//
+//        if(height > 60 && height < 84) {
+//            System.out.println("Enjoy the ride");
+//        }else {
+//            System.out.println("You do not meet the requirements");
+//        }
+        //////// Checking String Equality ///////////
+        System.out.println("The Vip Lounge");
 
-       if(height < 60){
-           System.out.println("Sorry, you're too short");
-       } else if(height > 84) {
-           System.out.println("Sorry, you're too tall");
-       }
+        System.out.println("Age");
+        int age = sc.nextInt();
 
-       else {
-           System.out.println("Enjoy the ride");
-       }
+        System.out.println("Do you have a vip pass? yes/no");
+        String vipPassReply = sc.next();
+
+
+
+        if (age >= 18 && vipPassReply.equalsIgnoreCase("Yes")|| age>= 70) {
+            System.out.println("Thanks, go on in");
+        } else {
+            System.out.println("Sorry, can't let you in.");
+        }
     }
 }
 
