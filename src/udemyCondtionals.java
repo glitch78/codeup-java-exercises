01bc01ee1f2e1218bec1fd5baef6a956849201eb
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-21 12:33:02 -0500

    add udemy conditionals

---
+++ src/udemyCondtionals.java
@@ -0,0 +1,20 @@
+
+
+public class udemyCondtionals {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+        if(10 < 5){
+            System.out.println(4);
+            System.out.println(5);
+            System.out.println(6);
+        }
+
+        System.out.println(7);
+        System.out.println(8);
+        System.out.println(9);
+    }
+}
+
