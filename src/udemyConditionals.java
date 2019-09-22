132b86d4c0649a98e3fb176af62d2df77f392698
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-22 18:20:53 -0500

    add updated to udemy conditionals

--- src/udemyConditionals.java
+++ src/udemyConditionals.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.Random;
 import java.util.Scanner;
 
-public class udemyCondtionals {
+public class udemyConditionals {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
